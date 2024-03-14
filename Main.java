31a26298de515d49112880af714c5ae8b0097426
WkGGs <150283796+WkGGs@users.noreply.github.com>
2024-03-15 00:18:09 +0300

    v2
    добавлен трим для различных версий телефонов. улучшена замена цифр на звездочки - больше нет привязке к if и длине строчки.

--- Main.java
+++ Main.java
@@ -2,11 +2,9 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class Main {
     public static void main(String[] args) {
-        String phoneNumber = new String("89944366792");
-        System.out.println(checkNumber(phoneNumber));
-        System.out.println("Вова, я наконец-то пришле в себя и справился с этим говном");
+        String phoneNumber = new String("+7(994)436 67-92");
         if(checkNumber(phoneNumber)){
-            System.out.println(maskPlacer(phoneNumber));
+            System.out.println(maskPlacer(phoneNumber.replaceAll("[ ()-]", "")));
         }
     }
     static boolean checkNumber(String number){
@@ -15,16 +13,7 @@ public class Main {
         return (m.matches());
     }
     static String maskPlacer(String number){
-        String maskedNumber = new String();
-        if(number.length() == 11){
-            maskedNumber = number.substring(0, 1) + "******" + number.substring(7);
-        }
-        if(number.length() == 12){
-            maskedNumber = number.substring(0, 2) + "******" + number.substring(8);
-        }
-        else{
-            return ("неправильный номер");
-        }
+        String maskedNumber = number.substring(0,number.length()-10) + "******" + number.substring(number.length()-4);
         return(maskedNumber);
     }
 }
